b6e19c7f5daac6e9789e5cedd991b63006f234ae
Hao Lee <lihao@kylinos.cn>
2018-04-12 15:00:43 +0800

    [Minor] 更新翻译文件

--- i18n_ts/zh_CN.ts
+++ i18n_ts/zh_CN.ts
@@ -92,7 +92,7 @@
     </message>
     <message>
         <location filename="../contentpane.cpp" line="75"/>
-        <location filename="../contentpane.cpp" line="311"/>
+        <location filename="../contentpane.cpp" line="302"/>
         <source>Feature name</source>
         <translation>特征名称</translation>
     </message>
@@ -102,277 +102,202 @@
         <translation>索引</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="100"/>
-        <source>Enabled</source>
-        <translation>已启用</translation>
-    </message>
-    <message>
-        <location filename="../contentpane.cpp" line="103"/>
+        <location filename="../contentpane.cpp" line="96"/>
         <source>Device is not connected</source>
         <translation>设备未连接</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="105"/>
-        <source>Disabled</source>
-        <translation>未启用</translation>
+        <location filename="../contentpane.cpp" line="94"/>
+        <source>Device is available</source>
+        <translation>设备可用</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="133"/>
+        <location filename="../contentpane.cpp" line="124"/>
         <source>Fingerprint</source>
         <translation>指纹</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="135"/>
+        <location filename="../contentpane.cpp" line="126"/>
         <source>Fingervein</source>
         <translation>指静脉</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="137"/>
+        <location filename="../contentpane.cpp" line="128"/>
         <source>Iris</source>
         <translation>虹膜</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="141"/>
+        <location filename="../contentpane.cpp" line="132"/>
         <source>Hardware Verification</source>
         <translation>硬件验证</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="143"/>
+        <location filename="../contentpane.cpp" line="134"/>
         <source>Software Verification</source>
         <translation>软件验证</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="145"/>
+        <location filename="../contentpane.cpp" line="136"/>
         <source>Mix Verification</source>
         <translation>混合验证</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="147"/>
+        <location filename="../contentpane.cpp" line="138"/>
         <source>Other Verification</source>
         <translation>其他验证</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="151"/>
+        <location filename="../contentpane.cpp" line="142"/>
         <source>Serial</source>
         <translation>串口</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="153"/>
+        <location filename="../contentpane.cpp" line="144"/>
         <source>USB</source>
         <translation>USB</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="155"/>
+        <location filename="../contentpane.cpp" line="146"/>
         <source>PCIE</source>
         <translation>PCIE</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="159"/>
+        <location filename="../contentpane.cpp" line="150"/>
         <source>Device Storage</source>
         <translation>设备存储</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="161"/>
+        <location filename="../contentpane.cpp" line="152"/>
         <source>OS Storage</source>
         <translation>系统存储</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="163"/>
+        <location filename="../contentpane.cpp" line="154"/>
         <source>Mix Storage</source>
         <translation>混合存储</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="167"/>
+        <location filename="../contentpane.cpp" line="158"/>
         <source>Hardware Identification</source>
         <translation>硬件识别</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="169"/>
+        <location filename="../contentpane.cpp" line="160"/>
         <source>Software Identification</source>
         <translation>软件识别</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="171"/>
+        <location filename="../contentpane.cpp" line="162"/>
         <source>Mix Identification</source>
         <translation>混合识别</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="173"/>
+        <location filename="../contentpane.cpp" line="164"/>
         <source>Other Identification</source>
         <translation>其他识别</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="308"/>
+        <location filename="../contentpane.cpp" line="299"/>
         <source>OK</source>
         <translation>确认</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="309"/>
+        <location filename="../contentpane.cpp" line="300"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="310"/>
+        <location filename="../contentpane.cpp" line="301"/>
         <source>Please input a feature name</source>
         <translation>请输入特征名称</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="328"/>
-        <location filename="../contentpane.cpp" line="456"/>
+        <location filename="../contentpane.cpp" line="319"/>
+        <location filename="../contentpane.cpp" line="447"/>
         <source>Permission is required. Please authenticate yourself to continue</source>
         <translation>需要授权！请先进行认证以继续操作</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="360"/>
+        <location filename="../contentpane.cpp" line="351"/>
         <source>Enroll successfully</source>
         <translation>录入成功</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="370"/>
-        <location filename="../contentpane.cpp" line="578"/>
-        <location filename="../contentpane.cpp" line="661"/>
+        <location filename="../contentpane.cpp" line="361"/>
+        <location filename="../contentpane.cpp" line="569"/>
+        <location filename="../contentpane.cpp" line="652"/>
         <source>D-Bus calling error</source>
         <translation>D-Bus 调用错误</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="376"/>
+        <location filename="../contentpane.cpp" line="367"/>
         <source>Failed to enroll</source>
         <translation>录入失败</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="378"/>
-        <location filename="../contentpane.cpp" line="586"/>
-        <location filename="../contentpane.cpp" line="669"/>
+        <location filename="../contentpane.cpp" line="369"/>
+        <location filename="../contentpane.cpp" line="577"/>
+        <location filename="../contentpane.cpp" line="660"/>
         <source>Device encounters an error</source>
         <translation>设备遇到错误</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="382"/>
-        <location filename="../contentpane.cpp" line="590"/>
-        <location filename="../contentpane.cpp" line="673"/>
+        <location filename="../contentpane.cpp" line="373"/>
+        <location filename="../contentpane.cpp" line="581"/>
+        <location filename="../contentpane.cpp" line="664"/>
         <source>Operation timeout</source>
         <translation>操作超时</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="387"/>
-        <location filename="../contentpane.cpp" line="594"/>
-        <location filename="../contentpane.cpp" line="677"/>
+        <location filename="../contentpane.cpp" line="378"/>
+        <location filename="../contentpane.cpp" line="585"/>
+        <location filename="../contentpane.cpp" line="668"/>
         <source>Device is busy</source>
         <translation>设备忙</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="391"/>
-        <location filename="../contentpane.cpp" line="597"/>
-        <location filename="../contentpane.cpp" line="680"/>
+        <location filename="../contentpane.cpp" line="382"/>
+        <location filename="../contentpane.cpp" line="588"/>
+        <location filename="../contentpane.cpp" line="671"/>
         <source>No such device</source>
         <translation>设备不存在</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="395"/>
-        <location filename="../contentpane.cpp" line="600"/>
-        <location filename="../contentpane.cpp" line="683"/>
+        <location filename="../contentpane.cpp" line="386"/>
+        <location filename="../contentpane.cpp" line="591"/>
+        <location filename="../contentpane.cpp" line="674"/>
         <source>Permission denied</source>
         <translation>没有权限</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="424"/>
+        <location filename="../contentpane.cpp" line="415"/>
         <source>Failed to get notify message</source>
         <translation>读取操作信息失败</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="470"/>
+        <location filename="../contentpane.cpp" line="461"/>
         <source>In progress, please wait...</source>
         <translation>操作中，请稍后...</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="569"/>
+        <location filename="../contentpane.cpp" line="560"/>
         <source>Match successfully</source>
         <translation>匹配成功</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="584"/>
+        <location filename="../contentpane.cpp" line="575"/>
         <source>Failed to match</source>
         <translation>匹配失败</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="650"/>
+        <location filename="../contentpane.cpp" line="641"/>
         <source>Found! Username: %1, Feature name: %2</source>
         <translation>搜索成功！用户名：%1，特征名称：%2</translation>
     </message>
     <message>
-        <location filename="../contentpane.cpp" line="667"/>
+        <location filename="../contentpane.cpp" line="658"/>
         <source>Not Found</source>
         <translation>未搜索到</translation>
-    </message>
-</context>
-<context>
-    <name>DriverInputDialog</name>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="23"/>
-        <location filename="../driverinputdialog.cpp" line="26"/>
-        <source>Required Field</source>
-        <translation>必填项</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="29"/>
-        <source>Optional Field</source>
-        <translation>可选项</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="34"/>
-        <source>Driver Name</source>
-        <translation>驱动名称</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="39"/>
-        <source>Status</source>
-        <translation>状态</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="43"/>
-        <source>Enable</source>
-        <translation>启用</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="44"/>
-        <source>Disable</source>
-        <translation>禁用</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="55"/>
-        <source>Driver Path</source>
-        <translation>驱动路径</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="60"/>
-        <source>Device Name</source>
-        <translation>设备路径</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="65"/>
-        <source>OK</source>
-        <translation>确认</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="66"/>
-        <source>Cancel</source>
-        <translation>取消</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="90"/>
-        <location filename="../driverinputdialog.cpp" line="99"/>
-        <source>Information</source>
-        <translation>提示信息</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="91"/>
-        <source>Driver Name Can&apos;t be Empty</source>
-        <translation>驱动名称不能为空</translation>
-    </message>
-    <message>
-        <location filename="../driverinputdialog.cpp" line="100"/>
-        <source>Driver Path Can&apos;t be Empty</source>
-        <translation>驱动路径不能为空</translation>
     </message>
 </context>
 <context>
@@ -395,82 +320,82 @@
     </message>
     <message>
         <location filename="../mainwindow.ui" line="83"/>
-        <source>Service Management</source>
-        <translation>服务管理</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="95"/>
-        <source>Service Status:</source>
-        <translation>服务状态：</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="105"/>
-        <source>Driver Management</source>
-        <translation>驱动管理</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="139"/>
         <source>Authentication Management</source>
         <translation>认证管理</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="147"/>
+        <location filename="../mainwindow.ui" line="91"/>
         <source>Biometric Authentication Status:</source>
         <translation>生物识别认证状态：</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="156"/>
+        <location filename="../mainwindow.ui" line="100"/>
         <source>Biometric Authentication can take over system authentication processes which include Login, Screensaver, sudo/su and Polkit.</source>
         <translation>生物识别可接管系统认证过程，包括登录、锁屏、sudo/su 授权和 Polkit 提权。</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="170"/>
-        <location filename="../mainwindow.cpp" line="407"/>
+        <location filename="../mainwindow.ui" line="113"/>
+        <source>Device Management</source>
+        <translation>设备管理</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="121"/>
+        <source>Fingerprint Devices</source>
+        <translation>指纹设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>Fingervein Devices</source>
+        <translation>指静脉设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="155"/>
+        <source>Iris Devices</source>
+        <translation>虹膜设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="174"/>
         <source>Fingerprint</source>
         <translation>指纹</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="186"/>
-        <location filename="../mainwindow.cpp" line="409"/>
+        <location filename="../mainwindow.ui" line="212"/>
+        <location filename="../mainwindow.ui" line="238"/>
+        <source>Driver Not Found</source>
+        <translation>驱动未找到</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="200"/>
         <source>Fingervein</source>
         <translation>指静脉</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="202"/>
-        <location filename="../mainwindow.cpp" line="411"/>
+        <location filename="../mainwindow.ui" line="226"/>
         <source>Iris</source>
         <translation>虹膜</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="61"/>
+        <location filename="../mainwindow.cpp" line="53"/>
+        <location filename="../mainwindow.cpp" line="394"/>
+        <location filename="../mainwindow.cpp" line="416"/>
         <source>Fatal Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="62"/>
+        <location filename="../mainwindow.cpp" line="54"/>
         <source>API version is not compatible</source>
         <translation>API 版本不兼容</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="397"/>
-        <source>No Any Device</source>
-        <translation>没有任何设备</translation>
+        <location filename="../mainwindow.cpp" line="395"/>
+        <source>Fail to change device status</source>
+        <translation>更该设备状态失败</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="566"/>
-        <location filename="../mainwindow.cpp" line="619"/>
-        <source>Error</source>
-        <translation>错误</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="567"/>
-        <source>Fail to add driver</source>
-        <translation>添加驱动失败</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="620"/>
-        <source>Fail to remove driver</source>
-        <translation>移除驱动失败</translation>
+        <location filename="../mainwindow.cpp" line="417"/>
+        <source>Device is not connected</source>
+        <translation>设备未连接</translation>
     </message>
 </context>
 <context>
