004acff78191b5b8ee408e844c5fc5ab30ab8d93
yanghao <yanghao@kylinos.cn>
2018-09-20 09:20:14 +0800

    [Lintian] fix spelling-error for ukui-pam-biometric-dialog

--- pam-biometric-dialog/src/main.cpp
+++ pam-biometric-dialog/src/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     QCommandLineOption serviceOption({"s", "service"}, QObject::tr("Sevice Name"));
     QCommandLineOption displayOption({"x", "display"}, QObject::tr("DISPLAY env"), "display", ":0");
     QCommandLineOption usernameOption({"u", "username"}, QObject::tr("User Name"), "username", "");
-    QCommandLineOption debugOption({"d", "debug"}, QObject::tr("Display debug infomation"));
+    QCommandLineOption debugOption({"d", "debug"}, QObject::tr("Display debug information"));
 
     parser.addOptions({serviceOption, displayOption, usernameOption, debugOption});
     parser.process(a);
