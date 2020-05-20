b0cfd6649392623de8d0f3795d1ee424d8552c34
Aylinux <cihanalk@gmail.com>
2020-05-20 04:55:12 +0300

    Create tr.ts (#6)

---
+++ bioauth/i18n_ts/tr.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>BioAuthWidget</name>
+    <message>
+        <location filename="../src/bioauthwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/bioauthwidget.ui" line="49"/>
+        <location filename="../src/bioauthwidget.ui" line="90"/>
+        <location filename="../src/bioauthwidget.ui" line="100"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/bioauthwidget.ui" line="140"/>
+        <source>More</source>
+        <translation>Daha Fazla</translation>
+    </message>
+    <message>
+        <location filename="../src/bioauthwidget.ui" line="156"/>
+        <source>Retry</source>
+        <translation>Yeniden Dene</translation>
+    </message>
+    <message>
+        <location filename="../src/bioauthwidget.ui" line="172"/>
+        <source>Password</source>
+        <translation>Parola</translation>
+    </message>
+    <message>
+        <location filename="../src/bioauthwidget.cpp" line="105"/>
+        <source>Current Device: </source>
+        <translation>Şuanki Aygıt：</translation>
+    </message>
+</context>
+<context>
+    <name>BioDevices</name>
+    <message>
+        <location filename="../src/biodevices.cpp" line="168"/>
+        <source>FingerPrint</source>
+        <translation>Parmak İzi</translation>
+    </message>
+    <message>
+        <location filename="../src/biodevices.cpp" line="170"/>
+        <source>FingerVein</source>
+        <translation>Damar İzi</translation>
+    </message>
+    <message>
+        <location filename="../src/biodevices.cpp" line="172"/>
+        <source>Iris</source>
+        <translation>Göz</translation>
+    </message>
+    <message>
+        <location filename="../src/biodevices.cpp" line="174"/>
+        <source>Face</source>
+        <translation>Yüz</translation>
+    </message>
+    <message>
+        <location filename="../src/biodevices.cpp" line="176"/>
+        <source>VoicePrint</source>
+        <translation>Ses İzi</translation>
+    </message>
+</context>
+<context>
+    <name>BioDevicesWidget</name>
+    <message>
+        <location filename="../src/biodeviceswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/biodeviceswidget.ui" line="24"/>
+        <source>Device types:</source>
+        <translation>Aygıt türü：</translation>
+    </message>
+    <message>
+        <location filename="../src/biodeviceswidget.ui" line="83"/>
+        <source>Back</source>
+        <translation>Geri</translation>
+    </message>
+    <message>
+        <location filename="../src/biodeviceswidget.ui" line="121"/>
+        <source>OK</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <source>FingerPrint</source>
+        <translation type="vanished">Parmak İzi</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>FingerPrint</source>
+        <translation type="vanished">Parmak İzi</translation>
+    </message>
+    <message>
+        <source>FingerVein</source>
+        <translation type="vanished">Damar İzi</translation>
+    </message>
+    <message>
+        <source>Iris</source>
+        <translation type="vanished">Göz</translation>
+    </message>
+    <message>
+        <source>Face</source>
+        <translation type="vanished">Yüz</translation>
+    </message>
+    <message>
+        <source>VoicePrint</source>
+        <translation type="vanished">Ses İzi</translation>
+    </message>
+</context>
+</TS>
