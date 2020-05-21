bd9cd926b7b63edc0e4e2b811ccfa96c6e4a17c2
Aylinux <cihanalk@gmail.com>
2020-05-21 04:49:22 +0300

    Create tr.ts (#7)

---
+++ polkit-agent/i18n_ts/tr.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="41"/>
+        <source>Authentication</source>
+        <translation>Kimlik Doğrulama</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="26"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>More</source>
+        <translation type="obsolete">Daha Fazla</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation type="obsolete">Yeniden Başlat</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="obsolete">Parola</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="156"/>
+        <source>Biometric</source>
+        <translation>Biyometrik</translation>
+    </message>
+    <message>
+        <source>DeviceType:</source>
+        <translation type="obsolete">Aygıt Türü：</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation type="obsolete">Geri</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="194"/>
+        <source>Details</source>
+        <translation>Ayrıntılar</translation>
+    </message>
+    <message>
+        <source>Action Id:</source>
+        <translation type="obsolete">İşlem Kimliği:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="332"/>
+        <source>Description:</source>
+        <translation>Açıklama：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="251"/>
+        <source>Polkit.subject-pid:</source>
+        <translation>Polkit.subject-pid：</translation>
+    </message>
+    <message>
+        <source>Retry</source>
+        <translation type="obsolete">Yeniden Dene</translation>
+    </message>
+    <message>
+        <source>Device types:</source>
+        <translation type="obsolete">Aygıt türü：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="274"/>
+        <source>Vendor:</source>
+        <translation>Vendor：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="365"/>
+        <source>Action:</source>
+        <translation>Eylem：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="342"/>
+        <source>Polkit.caller-pid:</source>
+        <translation>Polkit.caller-pid：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="407"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="423"/>
+        <source>Authenticate</source>
+        <translation>Kimlik Doğrulaması</translation>
+    </message>
+    <message>
+        <source>Auth</source>
+        <translation type="obsolete">Kimlik Doğrulaması</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="152"/>
+        <source>in authentication, please wait...</source>
+        <translation>kimlik doğrulanırken lütfen bekleyin ...</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="198"/>
+        <source>An application is attempting to perform an action that requires privileges. Authentication is required to perform this action.</source>
+        <translation>Bir uygulama, ayrıcalıklar gerektiren bir eylem gerçekleştirmeye çalışıyor. Bu işlemi gerçekleştirmek için kimlik doğrulaması gerekiyor.</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="233"/>
+        <source>Password: </source>
+        <translation type="unfinished">Parola： </translation>
+    </message>
+    <message>
+        <source>_Password: </source>
+        <translation type="obsolete">_Parola： </translation>
+    </message>
+    <message>
+        <source>_Password:</source>
+        <translation type="obsolete">_Parola：</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="250"/>
+        <source>Authentication failed, please try again.</source>
+        <translation>Kimlik doğrulama başarısız, lütfen tekrar deneyin.</translation>
+    </message>
+    <message>
+        <source>Authentication failed, please try again</source>
+        <translation type="obsolete">Kimlik doğrulama başarısız, lütfen tekrar deneyin</translation>
+    </message>
+</context>
+<context>
+    <name>PolkitListener</name>
+    <message>
+        <location filename="../src/PolkitListener.cpp" line="58"/>
+        <source>Another client is already authenticating, please try again later.</source>
+        <translation>Başka bir hesap zaten kimlik doğrulaması yapıyor, lütfen daha sonra tekrar deneyin.</translation>
+    </message>
+    <message>
+        <location filename="../src/PolkitListener.cpp" line="210"/>
+        <source>Authentication failure, please try again.</source>
+        <translation>Kimlik doğrulama hatalı, lütfen tekrar deneyin.</translation>
+    </message>
+    <message>
+        <location filename="../src/PolkitListener.cpp" line="219"/>
+        <source>Account locked %1 minutes due to %2 fail attempts</source>
+        <translation>%2 başarısız denemeden dolayı hesap %1 dakika kilitlendi</translation>
+    </message>
+    <message>
+        <location filename="../src/PolkitListener.cpp" line="223"/>
+        <source>Authentication failure,there are still %1 remaining opportunities</source>
+        <translation>Kimlik doğrulama hatası,  %1 hakkınız kaldı</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>FingerPrint</source>
+        <translation type="obsolete">Parmak İzi</translation>
+    </message>
+    <message>
+        <source>FingerVein</source>
+        <translation type="obsolete">Damar İzi</translation>
+    </message>
+    <message>
+        <source>Iris</source>
+        <translation type="obsolete">Göz</translation>
+    </message>
+    <message>
+        <source>Face</source>
+        <translation type="obsolete">Yüz Tanıma</translation>
+    </message>
+    <message>
+        <source>VoicePrint</source>
+        <translation type="obsolete">Ses İzi</translation>
+    </message>
+</context>
+</TS>
