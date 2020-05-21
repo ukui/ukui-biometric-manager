13b73a1ef170a56e2fd60e972376db27ba9b988c
Aylinux <cihanalk@gmail.com>
2020-05-21 04:48:28 +0300

    Create tr.ts (#8)

---
+++ bioauth-bin/i18n_ts/tr.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>BioDevices</name>
+    <message>
+        <location filename="../../bioauth/src/biodevices.cpp" line="168"/>
+        <source>FingerPrint</source>
+        <translation type="unfinished">Parmak İzi</translation>
+    </message>
+    <message>
+        <location filename="../../bioauth/src/biodevices.cpp" line="170"/>
+        <source>FingerVein</source>
+        <translation type="unfinished">Damar İzi</translation>
+    </message>
+    <message>
+        <location filename="../../bioauth/src/biodevices.cpp" line="172"/>
+        <source>Iris</source>
+        <translation type="unfinished">Göz</translation>
+    </message>
+    <message>
+        <location filename="../../bioauth/src/biodevices.cpp" line="174"/>
+        <source>Face</source>
+        <translation type="unfinished">Yüz Tanıma</translation>
+    </message>
+    <message>
+        <location filename="../../bioauth/src/biodevices.cpp" line="176"/>
+        <source>VoicePrint</source>
+        <translation type="unfinished">Ses İzi</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="100"/>
+        <source>Try it again</source>
+        <translation>Yeniden Dene</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="102"/>
+        <source>Use other Devices</source>
+        <translation>Diğer Cihazları Kullan</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="103"/>
+        <source>Cancel</source>
+        <translation>İptal</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="112"/>
+        <source>Choose the option</source>
+        <translation>Seçeneği seçin</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="134"/>
+        <location filename="../src/main.cpp" line="169"/>
+        <location filename="../src/main.cpp" line="178"/>
+        <source>Invaild response &quot;</source>
+        <translation>Geçersiz cevap &quot;</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="135"/>
+        <location filename="../src/main.cpp" line="170"/>
+        <location filename="../src/main.cpp" line="179"/>
+        <location filename="../src/main.cpp" line="290"/>
+        <source>AUTHENTICATION CANCELED</source>
+        <translation>KİMLİK DOĞRULAMA</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="146"/>
+        <source>=== BIOMETRIC DEVICES ===</source>
+        <translation>=== BİYOMETRİK CİHAZLAR ===</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="157"/>
+        <source>Choose the device</source>
+        <translation>Cihazı seçin</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="210"/>
+        <source>Sevice Name</source>
+        <translation>Servis Adı</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="211"/>
+        <source>DISPLAY env</source>
+        <translation>EKRAN env</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="212"/>
+        <source>User</source>
+        <translation type="unfinished">Kullanıcı</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="213"/>
+        <source>Display debug information</source>
+        <translation>Hata ayıklama bilgilerini görüntüle</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="214"/>
+        <source>Device short name</source>
+        <translation type="unfinished">Cihaz kısa adı</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="244"/>
+        <source>BIOMETRIC AUTHENTICATION</source>
+        <translation>BİYOMETRİK KİMLİK DOĞRULAMA</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="245"/>
+        <source>Press Q or Esc to cancel</source>
+        <translation>İptal etmek için Q veya Esc tuşuna basın</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="257"/>
+        <source>AUTHENTICATION SUCCESS</source>
+        <translation>KİMLİK DOĞRULAMA</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="261"/>
+        <source>AUTHENTICATION FAILED</source>
+        <translation>KİMLİK DOĞRULAMA BAŞARISIZ OLDU</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="278"/>
+        <source>BIOMETRIC AUTHENTICATION END</source>
+        <translation>BİYOMETRİK KİMLİK DOĞRULAMA SONU</translation>
+    </message>
+</context>
+</TS>
