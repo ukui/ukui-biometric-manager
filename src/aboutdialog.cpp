e972e76cfee73c44d45e27df04eb1d9527940cf8
liudunfa <2393411518@qq.com>
2021-08-18 10:04:05 +0800

    帮助文档图片更新

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -57,7 +57,7 @@ QString AboutDialog::get_current_version()
     size_t len = 0;
     ssize_t read;
     char *q = NULL;
-    QString version = "1.0.1-1kylin1~39";
+    QString version = "1.0.1-1kylin1~41";
 
     pp = popen("dpkg -l  ukui-biometric-manager | grep  ukui-biometric-manager", "r");
     if(NULL == pp)
